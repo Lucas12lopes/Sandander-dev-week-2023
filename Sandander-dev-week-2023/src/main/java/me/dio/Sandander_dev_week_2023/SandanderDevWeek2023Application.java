1b4fd699fb940b4180058ade6ff41725aa075692
Lucas12lopes <lucaslopeschaves1997@gmail.com>
2025-01-02 08:05:25 -0300

    Initial commit

---
+++ Sandander-dev-week-2023/src/main/java/me/dio/Sandander_dev_week_2023/SandanderDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.Sandander_dev_week_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandanderDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandanderDevWeek2023Application.class, args);
+	}
+
+}
